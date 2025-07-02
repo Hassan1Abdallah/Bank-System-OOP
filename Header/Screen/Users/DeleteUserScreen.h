52e47d632ef5b5040d4343627b1374c48a2d3fb8
Hassan Abdallah Hassan <162519586+Hassan1Abdallah@users.noreply.github.com>
2025-07-03 00:37:39 +0300

    Update DeleteUserScreen.h

--- Header/Screen/Users/DeleteUserScreen.h
+++ Header/Screen/Users/DeleteUserScreen.h
@@ -5,39 +5,39 @@
 #include "Screen.h"
 #include "User.h"
 
-class clsDeleteUserScreen : protected clsScreen {
+class DeleteUserScreen : protected Screen {
 
 private:
-    static void _PrintUser(clsUser User) {
+    static void _printUser(User user) {
         cout << "\nUser Card:";
         cout << "\n___________________";
-        cout << "\nFirstName   : " << User.firstName;
-        cout << "\nLastName    : " << User.lastName;
-        cout << "\nFull Name   : " << User.fullName();
-        cout << "\nEmail       : " << User.email;
-        cout << "\nPhone       : " << User.phone;
-        cout << "\nUser Name   : " << User.UserName;
-        cout << "\nPassword    : " << User.Password;
-        cout << "\nPermissions : " << User.Permissions;
+        cout << "\nFirstName   : " << user.firstName;
+        cout << "\nLastName    : " << user.lastName;
+        cout << "\nFull Name   : " << user.fullName();
+        cout << "\nEmail       : " << user.email;
+        cout << "\nPhone       : " << user.phone;
+        cout << "\nUser Name   : " << user.UserName;
+        cout << "\nPassword    : " << user.Password;
+        cout << "\nPermissions : " << user.Permissions;
         cout << "\n___________________\n";
     }
 
 public:
-    static void ShowDeleteUserScreen() {
+    static void showDeleteUserScreen() {
 
-        _DrawScreenHeader("\tDelete User Screen");
+        _drawScreenHeader("\tDelete User Screen");
 
         string UserName = "";
 
         cout << "\nPlease Enter UserName: ";
-        UserName = clsInputValidate::ReadString();
-        while (!clsUser::IsUserExist(UserName)) {
+        UserName = InputValidate::readString();
+        while (!User::isUserExist(UserName)) {
             cout << "\nUser is not found, choose another one: ";
-            UserName = clsInputValidate::ReadString();
+            UserName = InputValidate::readString();
         }
 
-        clsUser User1 = clsUser::Find(UserName);
-        _PrintUser(User1);
+        User User1 = User::find(UserName);
+        _printUser(User1);
 
         cout << "\nAre you sure you want to delete this User y/n? ";
 
@@ -48,7 +48,7 @@ public:
 
             if (User1.Delete()) {
                 cout << "\nUser Deleted Successfully :-)\n";
-                _PrintUser(User1);
+                _printUser(User1);
             } else {
                 cout << "\nError User Was not Deleted\n";
             }
