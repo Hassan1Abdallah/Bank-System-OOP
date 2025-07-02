a2d1ff02df7c34fe4b6d7854bd2a114c9c867e3d
Hassan Abdallah Hassan <162519586+Hassan1Abdallah@users.noreply.github.com>
2025-07-03 00:36:45 +0300

    Update FindCurrencyScreen.h

--- Header/Screen/Currencies/FindCurrencyScreen.h
+++ Header/Screen/Currencies/FindCurrencyScreen.h
@@ -4,51 +4,51 @@
 #include "InputValidate.h"
 #include "Screen.h"
 
-class clsFindCurrencyScreen : protected clsScreen {
+class FindCurrencyScreen : protected Screen {
 
 private:
-    static void _PrintCurrency(clsCurrency Currency) {
+    static void _printCurrency(Currency currency) {
         cout << "\nCurrency Card:\n";
         cout << "_____________________________\n";
-        cout << "\nCountry    : " << Currency.Country();
-        cout << "\nCode       : " << Currency.CurrencyCode();
-        cout << "\nName       : " << Currency.CurrencyName();
-        cout << "\nRate(1$) = : " << Currency.Rate();
+        cout << "\ncountry    : " << currency.country();
+        cout << "\nCode       : " << currency.currencyCode();
+        cout << "\nName       : " << currency.currencyName();
+        cout << "\nRate(1$) = : " << currency.rate();
 
         cout << "\n_____________________________\n";
     }
 
-    static void _ShowResults(clsCurrency Currency) {
-        if (!Currency.IsEmpty()) {
+    static void _showResults(Currency Currency) {
+        if (!Currency.isEmpty()) {
             cout << "\nCurrency Found :-)\n";
-            _PrintCurrency(Currency);
+            _printCurrency(Currency);
         } else {
             cout << "\nCurrency Was not Found :-(\n";
         }
     }
 
 public:
-    static void ShowFindCurrencyScreen() {
+    static void showFindCurrencyScreen() {
 
-        _DrawScreenHeader("\t  Find Currency Screen");
+        _drawScreenHeader("\t  find Currency Screen");
 
-        cout << "\nFind By: [1] Code or [2] Country ? ";
+        cout << "\nFind By: [1] Code or [2] country ? ";
         short Answer = 1;
 
         cin >> Answer;
 
         if (Answer == 1) {
-            string CurrencyCode;
-            cout << "\nPlease Enter CurrencyCode: ";
-            CurrencyCode = clsInputValidate::ReadString();
-            clsCurrency Currency = clsCurrency::FindByCode(CurrencyCode);
-            _ShowResults(Currency);
+            string currencyCode;
+            cout << "\nPlease Enter currencyCode: ";
+            currencyCode = InputValidate::readString();
+            Currency Currency = Currency::findByCode(currencyCode);
+            _showResults(Currency);
         } else {
-            string Country;
-            cout << "\nPlease Enter Country Name: ";
-            Country = clsInputValidate::ReadString();
-            clsCurrency Currency = clsCurrency::FindByCountry(Country);
-            _ShowResults(Currency);
+            string country;
+            cout << "\nPlease Enter country Name: ";
+            country = InputValidate::readString();
+            Currency Currency = Currency::findBycountry(country);
+            _showResults(Currency);
         }
     }
 };
