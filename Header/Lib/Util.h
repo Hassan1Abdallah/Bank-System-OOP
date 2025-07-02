49d8d221f0768194d9303bf57e44b55fd4e07135
Hassan Abdallah Hassan <162519586+Hassan1Abdallah@users.noreply.github.com>
2025-07-03 00:30:39 +0300

    Update Util.h

--- Header/Lib/Util.h
+++ Header/Lib/Util.h
@@ -8,53 +8,53 @@ using namespace std;
 class clsUtil {
 
 public:
-    enum enCharType { SamallLetter = 1, CapitalLetter = 2, Digit = 3, MixChars = 4, SpecialCharacter = 5 };
+    enum enCharType { samallLetter = 1, capitalLetter = 2, digit = 3, MixChars = 4, specialCharacter = 5 };
 
     static void Srand() {
         // Seeds the random number generator in C++, called only once
         srand((unsigned) time(NULL));
     }
 
-    static int RandomNumber(int From, int To) {
+    static int randomnumber(int From, int To) {
         // Function to generate a random number
         int randNum = rand() % (To - From + 1) + From;
         return randNum;
     }
 
-    static char GetRandomCharacter(enCharType CharType) {
+    static char getrandomCharacter(enCharType CharType) {
 
         // updated this method to accept mixchars
         if (CharType == MixChars) {
-            // Capital/Samll/Digits only
-            CharType = (enCharType) RandomNumber(1, 3);
+            // capital/Samll/digits only
+            CharType = (enCharType) randomnumber(1, 3);
         }
 
         switch (CharType) {
 
-            case enCharType::SamallLetter: {
-                return char(RandomNumber(97, 122));
+            case enCharType::samallLetter: {
+                return char(randomnumber(97, 122));
                 break;
             }
-            case enCharType::CapitalLetter: {
-                return char(RandomNumber(65, 90));
+            case enCharType::capitalLetter: {
+                return char(randomnumber(65, 90));
                 break;
             }
-            case enCharType::SpecialCharacter: {
-                return char(RandomNumber(33, 47));
+            case enCharType::specialCharacter: {
+                return char(randomnumber(33, 47));
                 break;
             }
-            case enCharType::Digit: {
-                return char(RandomNumber(48, 57));
+            case enCharType::digit: {
+                return char(randomnumber(48, 57));
                 break;
             }
             defualt: {
-                return char(RandomNumber(65, 90));
+                return char(randomnumber(65, 90));
                 break;
             }
         }
     }
 
-    static string GenerateWord(enCharType CharType, short Length)
+    static string generateWord(enCharType CharType, short Length)
 
     {
         string Word;
@@ -63,48 +63,48 @@ public:
 
         {
 
-            Word = Word + GetRandomCharacter(CharType);
+            Word = Word + getrandomCharacter(CharType);
         }
         return Word;
     }
 
-    static string GenerateKey(enCharType CharType = CapitalLetter) {
+    static string generateKey(enCharType CharType = capitalLetter) {
 
         string Key = "";
 
 
-        Key = GenerateWord(CharType, 4) + "-";
-        Key = Key + GenerateWord(CharType, 4) + "-";
-        Key = Key + GenerateWord(CharType, 4) + "-";
-        Key = Key + GenerateWord(CharType, 4);
+        Key = generateWord(CharType, 4) + "-";
+        Key = Key + generateWord(CharType, 4) + "-";
+        Key = Key + generateWord(CharType, 4) + "-";
+        Key = Key + generateWord(CharType, 4);
 
 
         return Key;
     }
 
-    static void GenerateKeys(short NumberOfKeys, enCharType CharType) {
+    static void generateKeys(short numberOfKeys, enCharType CharType) {
 
-        for (int i = 1; i <= NumberOfKeys; i++)
+        for (int i = 1; i <= numberOfKeys; i++)
 
         {
             cout << "Key [" << i << "] : ";
-            cout << GenerateKey(CharType) << endl;
+            cout << generateKey(CharType) << endl;
         }
     }
 
-    static void FillArrayWithRandomNumbers(int arr[100], int arrLength, int From, int To) {
+    static void fillArrayWithrandomnumbers(int arr[100], int arrLength, int From, int To) {
         for (int i = 0; i < arrLength; i++)
-            arr[i] = RandomNumber(From, To);
+            arr[i] = randomnumber(From, To);
     }
 
-    static void FillArrayWithRandomWords(string arr[100], int arrLength, enCharType CharType, short Wordlength) {
+    static void fillArrayWithrandomWords(string arr[100], int arrLength, enCharType CharType, short Wordlength) {
         for (int i = 0; i < arrLength; i++)
-            arr[i] = GenerateWord(CharType, Wordlength);
+            arr[i] = generateWord(CharType, Wordlength);
     }
 
-    static void FillArrayWithRandomKeys(string arr[100], int arrLength, enCharType CharType) {
+    static void fillArrayWithrandomKeys(string arr[100], int arrLength, enCharType CharType) {
         for (int i = 0; i < arrLength; i++)
-            arr[i] = GenerateKey(CharType);
+            arr[i] = generateKey(CharType);
     }
 
     static void Swap(int &A, int &B) {
@@ -147,26 +147,26 @@ public:
         B = Temp;
     }
 
-    static void Swap(clsDate &A, clsDate &B) { clsDate::SwapDates(A, B); }
+    static void Swap(Date &A, Date &B) { Date::swapDates(A, B); }
 
-    static void ShuffleArray(int arr[100], int arrLength) {
+    static void shuffleArray(int arr[100], int arrLength) {
 
         for (int i = 0; i < arrLength; i++) {
-            Swap(arr[RandomNumber(1, arrLength) - 1], arr[RandomNumber(1, arrLength) - 1]);
+            Swap(arr[randomnumber(1, arrLength) - 1], arr[randomnumber(1, arrLength) - 1]);
         }
     }
 
-    static void ShuffleArray(string arr[100], int arrLength) {
+    static void shuffleArray(string arr[100], int arrLength) {
 
         for (int i = 0; i < arrLength; i++) {
-            Swap(arr[RandomNumber(1, arrLength) - 1], arr[RandomNumber(1, arrLength) - 1]);
+            Swap(arr[randomnumber(1, arrLength) - 1], arr[randomnumber(1, arrLength) - 1]);
         }
     }
 
-    static string Tabs(short NumberOfTabs) {
+    static string Tabs(short numberOfTabs) {
         string t = "";
 
-        for (int i = 1; i < NumberOfTabs; i++) {
+        for (int i = 1; i < numberOfTabs; i++) {
             t = t + "\t";
             cout << t;
         }
@@ -174,53 +174,53 @@ public:
     }
 
 
-    static string NumberToText(int Number) {
+    static string numberToText(int number) {
 
-        if (Number == 0) {
+        if (number == 0) {
             return "";
         }
 
-        if (Number >= 1 && Number <= 19) {
+        if (number >= 1 && number <= 19) {
             string arr[] = {"",         "One",     "Two",     "Three",     "Four",     "Five",    "Six",
                             "Seven",    "Eight",   "Nine",    "Ten",       "Eleven",   "Twelve",  "Thirteen",
                             "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
 
-            return arr[Number] + " ";
+            return arr[number] + " ";
         }
 
-        if (Number >= 20 && Number <= 99) {
+        if (number >= 20 && number <= 99) {
             string arr[] = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-            return arr[Number / 10] + " " + NumberToText(Number % 10);
+            return arr[number / 10] + " " + numberToText(number % 10);
         }
 
-        if (Number >= 100 && Number <= 199) {
-            return "One Hundred " + NumberToText(Number % 100);
+        if (number >= 100 && number <= 199) {
+            return "One Hundred " + numberToText(number % 100);
         }
 
-        if (Number >= 200 && Number <= 999) {
-            return NumberToText(Number / 100) + "Hundreds " + NumberToText(Number % 100);
+        if (number >= 200 && number <= 999) {
+            return numberToText(number / 100) + "Hundreds " + numberToText(number % 100);
         }
 
-        if (Number >= 1000 && Number <= 1999) {
-            return "One Thousand " + NumberToText(Number % 1000);
+        if (number >= 1000 && number <= 1999) {
+            return "One Thousand " + numberToText(number % 1000);
         }
 
-        if (Number >= 2000 && Number <= 999999) {
-            return NumberToText(Number / 1000) + "Thousands " + NumberToText(Number % 1000);
+        if (number >= 2000 && number <= 999999) {
+            return numberToText(number / 1000) + "Thousands " + numberToText(number % 1000);
         }
 
-        if (Number >= 1000000 && Number <= 1999999) {
-            return "One Million " + NumberToText(Number % 1000000);
+        if (number >= 1000000 && number <= 1999999) {
+            return "One Million " + numberToText(number % 1000000);
         }
 
-        if (Number >= 2000000 && Number <= 999999999) {
-            return NumberToText(Number / 1000000) + "Millions " + NumberToText(Number % 1000000);
+        if (number >= 2000000 && number <= 999999999) {
+            return numberToText(number / 1000000) + "Millions " + numberToText(number % 1000000);
         }
 
-        if (Number >= 1000000000 && Number <= 1999999999) {
-            return "One Billion " + NumberToText(Number % 1000000000);
+        if (number >= 1000000000 && number <= 1999999999) {
+            return "One Billion " + numberToText(number % 1000000000);
         } else {
-            return NumberToText(Number / 1000000000) + "Billions " + NumberToText(Number % 1000000000);
+            return numberToText(number / 1000000000) + "Billions " + numberToText(number % 1000000000);
         }
     }
 
