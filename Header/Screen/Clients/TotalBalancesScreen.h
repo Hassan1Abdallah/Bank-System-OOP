225a98f053b29340955dcc25c30e976df521d748
Hassan Abdallah Hassan <162519586+Hassan1Abdallah@users.noreply.github.com>
2025-07-03 00:33:25 +0300

    Update TotalBalancesScreen.h

--- Header/Screen/Clients/TotalBalancesScreen.h
+++ Header/Screen/Clients/TotalBalancesScreen.h
@@ -5,32 +5,32 @@
 #include "Screen.h"
 #include "Util.h"
 
-class clsTotalBalancesScreen : protected clsScreen {
+class TotalBalancesScreen : protected Screen {
 
 private:
-    static void PrintClientRecordBalanceLine(BankClient Client) {
+    static void printClientRecordBalanceLine(BankClient Client) {
         cout << setw(25) << left << ""
-             << "| " << setw(15) << left << Client.getAccountNumber();
+             << "| " << setw(15) << left << Client.getAccountnumber();
         cout << "| " << setw(40) << left << Client.fullName();
         cout << "| " << setw(12) << left << Client.balance;
     }
 
 public:
-    static void ShowTotalBalances() {
+    static void showTotalBalances() {
 
         vector<BankClient> vClients = BankClient::getClientsList();
 
         string Title = "\t  Balances List Screen";
         string SubTitle = "\t    (" + to_string(vClients.size()) + ") Client(s).";
 
-        _DrawScreenHeader(Title, SubTitle);
+        _drawScreenHeader(Title, SubTitle);
 
         cout << setw(25) << left << ""
              << "\n\t\t_______________________________________________________";
         cout << "__________________________\n" << endl;
 
         cout << setw(25) << left << ""
-             << "| " << left << setw(15) << "Accout Number";
+             << "| " << left << setw(15) << "Accout number";
         cout << "| " << left << setw(40) << "Client Name";
         cout << "| " << left << setw(12) << "Balance";
         cout << setw(25) << left << ""
@@ -44,7 +44,7 @@ public:
         else
 
             for (BankClient Client: vClients) {
-                PrintClientRecordBalanceLine(Client);
+                printClientRecordBalanceLine(Client);
                 cout << endl;
             }
 
@@ -55,6 +55,6 @@ public:
         cout << setw(8) << left << ""
              << "\t\t\t\t\t\t\t     Total Balances = " << TotalBalances << endl;
         cout << setw(8) << left << ""
-             << "\t\t\t\t  ( " << clsUtil::NumberToText(TotalBalances) << ")";
+             << "\t\t\t\t  ( " << clsUtil::numberToText(TotalBalances) << ")";
     }
 };
